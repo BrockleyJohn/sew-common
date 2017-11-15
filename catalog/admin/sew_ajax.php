106486958c9c05b60898b4468a43bbcfacedb2e4
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2017-11-15 08:08:11 +0000

    Streamline input filtering and add referring domain check

--- catalog/admin/sew_ajax.php
+++ catalog/admin/sew_ajax.php
@@ -14,27 +14,60 @@
 
   Released under the GNU General Public License
 */
- 
   require('includes/application_top.php');
+  
+  // app top will have diverted any not signed in, but
+  // only process requests from pages from this site
+  $address = ($request_type == 'NONSSL' ? 'http' : 'https') . '://' . $_SERVER['SERVER_NAME'];
+  if (isset($_SERVER['HTTP_ORIGIN'])) {
+	if (strpos($address, $_SERVER['HTTP_ORIGIN']) !== 0) {
+		header('HTTP/1.0 403 Forbidden', true, 403);
+		header('Content-Type: application/json');
+		exit(json_encode([
+			'error' => 'Invalid Origin header: ' . $_SERVER['HTTP_ORIGIN']
+		]));
+	}
+  } elseif (isset($_SERVER['HTTP_REFERER'])) { // no origin so check the referrer
+	if (parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) <> $_SERVER['SERVER_NAME']) { // slightly better than not bothering
+		header('HTTP/1.0 403 Forbidden', true, 403);
+		header('Content-Type: application/json');
+		exit(json_encode([
+			'error' => 'Invalid Referer header: ' . $_SERVER['HTTP_REFERER']
+		]));
+	}
+  } else { 
+	header('HTTP/1.0 403 Forbidden', true, 403);
+	header('Content-Type: application/json');
+	exit(json_encode(['error' => 'No Origin/Referer headers']));
+  }  
+
+  function clean_var($var,$filter) {
+    $return = '';
+	if (isset($_POST[$var]) && $_POST[$var] <> '') {
+	  $return = filter_input ( INPUT_POST, $var, $filter );
+	} elseif (isset($_GET[$var]) && $_GET[$var] <> '') {
+	  $return = filter_input ( INPUT_GET, $var, $filter );
+	}
+	return $return;
+  }
+ 
 	$logging = '';
 //	$log = ((isset($_POST['log']) || isset($_POST['log'])) ? true : false);
-	if (isset($_POST['pagename']) && $_POST['pagename'] <> '') {
-	  $pagename = filter_input ( INPUT_POST, 'pagename', FILTER_SANITIZE_URL );
-	} elseif (isset($_GET['pagename']) && $_GET['pagename'] <> '') {
-	  $pagename = filter_input ( INPUT_GET, 'pagename', FILTER_SANITIZE_URL );
-	}
+
+	$pagename = clean_var('pagename',FILTER_SANITIZE_URL);
+	$action = clean_var('action',FILTER_SANITIZE_URL);
+
 	if (strlen($pagename)) {
 	  $logging .= 'including includes/languages/' . $language . '/' . basename($pagename)."\n";
-  if (file_exists('includes/languages/' . $language . '/' . basename($pagename))) {
-    include('includes/languages/' . $language . '/' . basename($pagename));
-  }
+	  if (file_exists('includes/languages/' . $language . '/' . basename($pagename))) {
+		include('includes/languages/' . $language . '/' . basename($pagename));
+	  }
 	}
  // include_once('includes/languages/' . $language . '/sew_ajax.php');
 
 	$result = array();
-	$action = (isset($_POST['action']) ? $_POST['action'] : $_GET['action']);
 
-  if (file_exists('sew_ajax/' . $action  . '.php')) {
+  if (strlen($action) && file_exists('sew_ajax/' . $action  . '.php')) {
 		if (file_exists('includes/languages/' . $language . '/sew_ajax/' . $action  . '.php')) include('includes/languages/' . $language . '/sew_ajax/' . $action  . '.php');
 		include('sew_ajax/' . $action  . '.php');
 	} else {
@@ -56,6 +89,8 @@
 		$result['get'] = $text;
 		$result['log'] = $logging;
 	}
+
+	header('Content-Type: application/json');
 	echo json_encode($result);
 
   require('includes/application_bottom.php');
